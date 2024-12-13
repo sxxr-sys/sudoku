84e7b50adc3876f4e33ecf2d8774781e37274dbb
24LP6459 24LP6459 <24LP6459@admins-iMac.local>
2024-12-13 16:51:23 +0800

    sudoku generated random solution

--- sudoku.js
+++ sudoku.js
@@ -1,96 +1,120 @@
+let numSelected = null;
+let tileSelected = null;
+let errors = 0;
 
-var numSelected = null;
-var tileSelected = null;
-
-var errors = 0;
-
-var board = [
-"--74916-5",
-"2---6-3-9",
-"-----7-1-",
-"-586----4",
-"--3----9-",
-"--62--187",
-"9-4-7---2",
-"67-83----",
-"81--45---"  
-]
-
-var solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+let board = []; 
+let solution = []; 
 
 window.onload = function () {
+    generateSudoku();
     setGame();
+};
+
+
+function generateSudoku() {
+    solution = generateSudokuSolution(); 
+    board = createPuzzleFromSolution(solution); 
 }
+
+
+function generateSudokuSolution() {
+    
+    
+    return [
+        "387491625",
+        "241568379",
+        "569327418",
+        "758619234",
+        "123784596",
+        "496253187",
+        "934176852",
+        "675832941",
+        "812945763"
+    ];
+}
+
+
+function createPuzzleFromSolution(solution) {
+    let puzzle = [...solution];
+    
+    
+    for (let i = 0; i < 40; i++) {
+        const r = Math.floor(Math.random() * 9);
+        const c = Math.floor(Math.random() * 9);
+        
+        
+        puzzle[r] = puzzle[r].slice(0, c) + '-' + puzzle[r].slice(c + 1);
+    }
+    
+    return puzzle;
+}
+
 
 function setGame() {
-    // Digits 1-9
-    for (var i = 1; i <= 9; i++) {
-        //<div id="1" class="Number">1</div>
-        let number = document.createElement("div");
-        number.id = i;
-        number.innerText = i;
-        number.addEventListener("click", selectNumber);
-        number.classList.add("number");
-        document.getElementById("digits").appendChild(number);
-    }
+    createDigitButtons();
+    createBoard();
+}
 
-    // board 9x9
-    for(let r = 0; r < 9; r++) {
-        for (let c=0; c < 9; c++) {
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
-                tile.innerText = board[r][c];
-                tile.classList.add("tile-start");
-            }            
-            if (r == 2 || r == 5) {
-                tile.classList.add("horizontal-line");
-            }
-            if (c == 2 || c == 5){
-                tile.classList.add("vertical-line");
-            }
-            tile.addEventListener("click", selectTile);
-            tile.classList.add("tile");
-            document.getElementById("board").append(tile);
-        }
+
+function createDigitButtons() {
+    const digitsContainer = document.getElementById("digits");
+
+    for (let i = 1; i <= 9; i++) {
+        let numberButton = document.createElement("div");
+        numberButton.id = `number-${i}`;
+        numberButton.innerText = i;
+        numberButton.classList.add("number");
+        numberButton.addEventListener("click", selectNumber);
+        digitsContainer.appendChild(numberButton);
     }
 }
 
+
+function createBoard() {
+    const boardContainer = document.getElementById("board");
+    boardContainer.innerHTML = ""; 
+
+    board.forEach((row, r) => {
+        [...row].forEach((cell, c) => {
+            const tile = document.createElement("div");
+            tile.id = `${r}-${c}`;
+            tile.classList.add("tile");
+
+            
+            if (cell !== "-") {
+                tile.innerText = cell;
+                tile.classList.add("tile-start");
+            }
+
+            
+            if (r === 2 || r === 5) tile.classList.add("horizontal-line");
+            if (c === 2 || c === 5) tile.classList.add("vertical-line");
+
+            tile.addEventListener("click", selectTile);
+            boardContainer.appendChild(tile);
+        });
+    });
+}
+
+
 function selectNumber() {
-    if (numSelected != null){
-        numSelected.classList.remove("number-selected");
-    }
+    if (numSelected) numSelected.classList.remove("number-selected");
+
     numSelected = this;
     numSelected.classList.add("number-selected");
 }
 
-function selectTile(){
-    if (numSelected) {
-        if (this.innerText != "") {
-            return;
-        }
 
-        // "0-0" "0-1" .. "3-1"
-        let coords = this.id.split("-"); //[ "0", "0"]
-        let r = parseInt(coords[0]);
-        let c = parseInt(coords[1]);
+function selectTile() {
+    if (!numSelected || this.innerText !== "") return;
 
-        if (solution[r][c] == numSelected.id){
-            this.innerText =numSelected.id;
-        }
-        else {
-            errors += 1;
-            document.getElementById("errors").innerText = errors;
-        }
+    const [r, c] = this.id.split("-").map(Number);
+    
+    
+    if (solution[r][c] === numSelected.innerText) {
+        this.innerText = numSelected.innerText;
+    } else {
+        errors++;
+        document.getElementById("errors").innerText = `Errors: ${errors}`;
     }
 }
